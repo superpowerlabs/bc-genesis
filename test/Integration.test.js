f644dc4df482157889abf4f06a1d99e51479ef51
Francesco Sullo <sullof@gmail.com>
2023-01-14 14:55:22 -0800

    Scaffolding ready

---
+++ test/Integration.test.js
@@ -0,0 +1,24 @@
+const {expect} = require("chai");
+const { deployContractUpgradeable, deployContract, number} = require("./helpers");
+
+describe("Integration test", function () {
+  let myToken;
+  let bodyPartToken;
+  let factory;
+
+  let owner, holder1, holder2, holder3;
+
+  before(async function () {
+    [owner, holder1, holder2, holder3] = await ethers.getSigners();
+  });
+
+  beforeEach(async function () {
+
+
+  });
+
+  it("should verify the flow", async function () {
+
+  });
+
+});
