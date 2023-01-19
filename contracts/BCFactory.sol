5f3408e0c1d848897b5fea1ed58d3226c0cf87ab
roquesrolando <roquesrolando@hotmail.com>
2023-01-19 15:51:11 -0400

    first round testing the flow of the minting from factory

--- contracts/BCFactory.sol
+++ contracts/BCFactory.sol
@@ -47,27 +47,29 @@ contract BCFactory is Signable, OwnableUpgradeable, UUPSUpgradeable {
     super.setValidator(id, validator);
   }
 
-  function mintGenesis(address to, uint256 randomValue, bytes calldata signature) external {
-    if (!isSignedByValidator(0, hashGenesis(to, randomValue), signature))
-      revert InvalidSignature();
-    genesisToken.mint(to);
+  function mintGenesis(uint256 randomValue, bytes calldata signature) external {
+    if (!isSignedByValidator(0, hashGenesis(_msgSender(), randomValue), signature)) revert InvalidSignature();
+    genesisToken.mint(_msgSender());
   }
 
-  function mintOracle(address to, uint256 partId1,
+  function mintOracle(
+    uint256 partId1,
     uint256 partId2,
     uint256 partId3,
-    uint256 partId4, uint256 randomValue, bytes calldata signature) external {
-    if (!isSignedByValidator(0, hashOracle(to, partId1,
-     partId2,
-     partId3,
-    partId4,randomValue), signature))
+    uint256 partId4,
+    uint256 randomValue,
+    bytes calldata signature
+  ) external {
+    if (!isSignedByValidator(0, hashOracle(_msgSender(), partId1, partId2, partId3, partId4, randomValue), signature))
       revert InvalidSignature();
-    if (genesisToken.ownerOf(partId1) != _msgSender()
-      || genesisToken.ownerOf(partId2) != _msgSender()
-    || genesisToken.ownerOf(partId3) != _msgSender()
-     || genesisToken.ownerOf(partId4) != _msgSender()) revert NotGenesisOwner();
+    if (
+      genesisToken.ownerOf(partId1) != _msgSender() ||
+      genesisToken.ownerOf(partId2) != _msgSender() ||
+      genesisToken.ownerOf(partId3) != _msgSender() ||
+      genesisToken.ownerOf(partId4) != _msgSender()
+    ) revert NotGenesisOwner();
     _saveSignatureAsUsed(signature);
-    oracleToken.mint(to);
+    oracleToken.mint(_msgSender());
     uint256[] memory parts = new uint256[](4);
     parts[0] = partId1;
     parts[1] = partId2;
@@ -75,8 +77,6 @@ contract BCFactory is Signable, OwnableUpgradeable, UUPSUpgradeable {
     parts[3] = partId4;
     genesisToken.burnBatch(parts);
   }
-
-
 
   function _saveSignatureAsUsed(bytes memory _signature) internal {
     bytes32 key = bytes32(keccak256(abi.encodePacked(_signature)));
@@ -89,19 +89,16 @@ contract BCFactory is Signable, OwnableUpgradeable, UUPSUpgradeable {
     return _usedSignatures[key];
   }
 
-  function hashGenesis(
-    address to,
-    uint256 randomValue
-  ) public view returns (bytes32) {
+  function hashGenesis(address to, uint256 randomValue) public view returns (bytes32) {
     return
-    keccak256(
-      abi.encodePacked(
-        "\x19\x01", // EIP-191
-        block.chainid,
-        to,
-        randomValue
-      )
-    );
+      keccak256(
+        abi.encodePacked(
+          "\x19\x01", // EIP-191
+          block.chainid,
+          to,
+          randomValue
+        )
+      );
   }
 
   function hashOracle(
@@ -113,18 +110,17 @@ contract BCFactory is Signable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 randomValue
   ) public view returns (bytes32) {
     return
-    keccak256(
-      abi.encodePacked(
-        "\x19\x01", // EIP-191
-        block.chainid,
-        to,
-        partId1,
-        partId2,
-        partId3,
-        partId4,
-        randomValue
-      )
-    );
+      keccak256(
+        abi.encodePacked(
+          "\x19\x01", // EIP-191
+          block.chainid,
+          to,
+          partId1,
+          partId2,
+          partId3,
+          partId4,
+          randomValue
+        )
+      );
   }
-
 }
--- test/BCFactory.test.js
+++ test/BCFactory.test.js
@@ -1,15 +1,19 @@
 const {expect} = require("chai");
-const { deployContractUpgradeable, deployContract, number} = require("./helpers");
+const {deployContractUpgradeable, deployContract, number, signPackedData} = require("./helpers");
 
 describe("BCFactory", function () {
   let factory;
   let genesis;
   let oracle;
+  let validator0, validator1;
 
-  let owner, holder1, holder2, holder3;
+  let owner, holder1, holder2, holder3, minter;
+  let validator0PK = "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a";
+  let validator1PK = "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba";
+  let validator2PK = "0x3141592653589793238462643383279502884197169399375105820974944592";
 
   before(async function () {
-    [owner, holder1, holder2, holder3] = await ethers.getSigners();
+    [owner, holder1, holder2, holder3, validator0, validator1] = await ethers.getSigners();
 
     BCGenesisToken = await ethers.getContractFactory("BCGenesisToken");
     BCOracleToken = await ethers.getContractFactory("BCOracleToken");
@@ -19,12 +23,25 @@ describe("BCFactory", function () {
   async function initAndDeploy() {
     genesis = await upgrades.deployProxy(BCGenesisToken, ["https://api.blocto.app/v1/genesis/{id}"]);
     await genesis.deployed();
+    genesis.setMaxSupply(100);
 
     oracle = await upgrades.deployProxy(BCOracleToken, ["https://api.blocto.app/v1/oracle/{id}"]);
     await oracle.deployed();
+    oracle.setMaxSupply(100);
 
     factory = await upgrades.deployProxy(BCFactory, [genesis.address, oracle.address]);
     await factory.deployed();
+
+    expect(await factory.setValidator(0, validator0.address))
+      .emit(factory, "ValidatorSet")
+      .withArgs(0, validator0.address);
+    expect(await factory.setValidator(1, validator1.address))
+      .emit(factory, "ValidatorSet")
+      .withArgs(1, validator1.address);
+  }
+
+  async function getSignature(hash, privateKey) {
+    return signPackedData(hash, privateKey);
   }
 
   beforeEach(async function () {
@@ -32,36 +49,73 @@ describe("BCFactory", function () {
   });
 
   describe("setValidator", function () {
-    it.skip("should successfully set Validator", async function () {
+    it("should successfully set Validator", async function () {
+      expect(await factory.setValidator(0, validator0.address))
+        .emit(factory, "ValidatorSet")
+        .withArgs(0, validator0.address);
     });
   });
 
   describe("hashGenesis", function () {
-    it.skip("should successfully hash genesis", async function () {
-      const hash = await factory.hashGenesis(
-        holder1.address,
-        Math.random());
+    it("should successfully hash genesis", async function () {
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash = await factory.hashGenesis(holder1.address, rand);
       expect(hash).to.be.a("string");
     });
   });
 
   describe("mintGenesis", function () {
-    it("should fail to mint if not owner of parts", async function () {
-      // const random = Math.random();
-      // const sig1 = 
-      // factory.mintGenesis(holder1, random, sig1);
+    it("should mint parts", async function () {
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash = await factory.hashGenesis(holder1.address, rand);
+      const sign = await getSignature(hash, validator0PK);
+      await genesis.setFactory(factory.address, true);
+      const part = await factory.connect(holder1).mintGenesis(rand, sign);
+      expect(part.hash).to.exist;
     });
     it("should fail to mint if not signed", async function () {
-
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash = await factory.hashGenesis(holder2.address, rand);
+      const sign = await getSignature(hash, validator0PK);
+      await genesis.setFactory(factory.address, true);
+      await expect(factory.connect(holder1).mintGenesis(rand, sign)).revertedWith("InvalidSignature()");
     });
   });
 
   describe("mintOracle", function () {
-    it("should fail to mint if not owner of parts", async function () {
-
+    it("should successfully hash oracle", async function () {
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash1 = await factory.hashOracle(holder1.address, 1, 2, 3, 4, rand);
+      expect(hash1).to.be.a("string");
+    });
+    it("should mint oracle", async function () {
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash = await factory.hashGenesis(holder1.address, rand);
+      const sign = await getSignature(hash, validator0PK);
+      await genesis.setFactory(factory.address, true);
+      for (let x = 0; x < 4; x++) {
+        await factory.connect(holder1).mintGenesis(rand, sign);
+      }
+      expect(await genesis.balanceOf(holder1.address)).equal(4);
+      const hash1 = await factory.hashOracle(holder1.address, 1, 2, 3, 4, rand);
+      const sign1 = await getSignature(hash1, validator0PK);
+      await oracle.setFactory(factory.address, true);
+      const orac = await factory.connect(holder1).mintOracle(1, 2, 3, 4, rand, sign1);
+      expect(orac.hash).to.exist;
     });
     it("should fail to mint if not signed", async function () {
-
+      const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+      const hash = await factory.hashGenesis(holder1.address, rand);
+      const sign = await getSignature(hash, validator0PK);
+      await genesis.setFactory(factory.address, true);
+      for (let x = 0; x < 4; x++) {
+        await factory.connect(holder1).mintGenesis(rand, sign);
+      }
+      expect(await genesis.balanceOf(holder1.address)).equal(4);
+      const hash1 = await factory.hashOracle(holder1.address, 1, 2, 3, 4, rand);
+      const sign1 = await getSignature(hash1, validator0PK);
+      await oracle.setFactory(factory.address, true);
+      await expect(factory.connect(holder2).mintOracle(1, 2, 3, 4, rand, sign1)).revertedWith("InvalidSignature()");
     });
   });
 });
