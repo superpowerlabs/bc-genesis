71dfa53969997f286c052ef803296b488b843189
jerrybassat7 <jerry.bassat@upr.edu>
2023-01-20 14:28:52 -0400

    Show AuthorShip

--- contracts/mocks/MockFactory.sol
+++ contracts/mocks/MockFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-// Author : Francesco Sullo < francesco@superpower.io>
+// Author : Jerry Bassat < jerry@superpower.io>
 // (c) Superpower Labs Inc.
 
 
