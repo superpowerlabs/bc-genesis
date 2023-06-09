2f9b59086488b11be5fab0369ecbb4d106add820
Francesco Sullo <sullof@gmail.com>
2023-06-09 10:16:29 -0700

    Temporary change

--- contracts/BCFactory.sol
+++ contracts/BCFactory.sol
@@ -41,6 +41,7 @@ contract BCFactory is OwnableUpgradeable, UUPSUpgradeable {
 
   bytes32 public merkleRoot;
   bool public allowListMintingFinished;
+  uint private _rangeSize;
 
   function initialize(address genesis_, address oracle_) public initializer {
     __Ownable_init();
@@ -49,6 +50,7 @@ contract BCFactory is OwnableUpgradeable, UUPSUpgradeable {
     if (!IERC165Upgradeable(oracle_).supportsInterface(type(IERC721Upgradeable).interfaceId)) revert NotAndERC721(oracle_);
     genesisToken = BCGenesisToken(genesis_);
     oracleToken = BCOracleToken(oracle_);
+    _rangeSize = 40;
   }
 
   function _authorizeUpgrade(address newImplementation) internal virtual override onlyOwner {}
@@ -76,6 +78,61 @@ contract BCFactory is OwnableUpgradeable, UUPSUpgradeable {
     genesisToken.mint(_msgSender(), tokenId);
   }
 
+  function _reverseTransform(uint y) internal pure returns (uint) {
+    uint base = (y / rangeSize) * rangeSize;
+    uint mod = ((y - 1) % rangeSize) + 10;
+    uint ones = mod / 10;
+    uint tens = mod % 10;
+    uint r = base + ones + (tens * 4);
+    if (r % rangeSize == 0) {
+      r -= rangeSize;
+    }
+    return r;
+  }
+
+  function _encode(uint256[] memory arr) internal pure returns (uint256) {
+    uint256 res;
+    if (arr.length > 77) revert TooManyValues();
+    for (uint256 i = 0; i < arr.length; i++) {
+      if (arr[i] > 4) revert InvalidRarity();
+      res += arr[i] * (10**i);
+    }
+    return res;
+  }
+
+  function _decode(uint256 encoded, uint256 index) internal pure returns (uint256) {
+    uint256 val = encoded / (10**index);
+    return val % 10;
+  }
+
+  function _rarityByIndex(uint256 index_) internal view returns (uint256) {
+    uint256 elem = _rarityIndex[index_ / (rangeSize * 77)];
+    uint256 onElem = index_ % (rangeSize * 77);
+    uint256 remainder = onElem / rangeSize;
+    return decode(elem, remainder);
+  }
+
+  function _revert(uint y) internal pure returns (uint) {
+    uint factor = 13;
+    uint addend = 17;
+    uint base = (y - 1) / rangeSize;
+    uint diff = base * rangeSize;
+    y = y - diff;
+    uint factorInverse = 1;
+    for (uint i = 1; i <= rangeSize; i++) {
+      if ((factor * i) % rangeSize == 1) {
+        factorInverse = i;
+        break;
+      }
+    }
+    return diff + ((((y - 1 + rangeSize - addend) % rangeSize) * factorInverse) % rangeSize) + 1;
+  }
+
+  function _getPart(uint partId) internal pure returns (uint) {
+    uint reverted = _reverseTransform(_revert(row.tokenId));
+    return ((reverted - 1) % 4) ** 2;
+  }
+
   function _validateBodyParts(
     uint256 partId1,
     uint256 partId2,
@@ -88,7 +145,9 @@ contract BCFactory is OwnableUpgradeable, UUPSUpgradeable {
       genesisToken.ownerOf(partId3) != _msgSender() ||
       genesisToken.ownerOf(partId4) != _msgSender()
     ) revert NotGenesisOwner();
-    // TODO add consistency check for body parts in a following upgrade, when the body parts are defined
+    if (_part(id1) + _part(id2) + _part(id3) + _part(id4) != 14) {
+      revert NotAFullSet();
+    }
   }
 
   function mintOracle(
@@ -97,7 +156,7 @@ contract BCFactory is OwnableUpgradeable, UUPSUpgradeable {
     uint256 partId3,
     uint256 partId4
   ) external {
-    if (oracleToken.totalSupply() >= 1000) revert OracleMintingFinished();
+    if (oracleToken.totalSupply() > 999) revert OracleMintingFinished();
     _validateBodyParts(partId1, partId2, partId3, partId4);
     uint256 oracleId = oracleToken.mint(_msgSender());
     genesisToken.burnBatch([partId1, partId2, partId3, partId4]);
