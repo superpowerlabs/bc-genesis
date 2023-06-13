ecd13fb1304fe47e479e41439f4cd292ce9f3794
Francesco Sullo <sullof@gmail.com>
2023-06-13 14:44:55 -0700

    Improve flattener

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # must be run from the root
 
-#rm -rf artifacts
-#rm -rf cache
-npx hardhat compile
+npm run clean
+npm run compile
 
 CONTRACT=$1 GAS_LIMIT=$3 npx hardhat run scripts/upgrade.js --network $2
