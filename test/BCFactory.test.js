4f282ae5f97528dd0a1fcb53f776dbc11efc685f
Francesco Sullo <sullof@gmail.com>
2023-08-04 16:23:58 -0700

    Saving changes

--- test/BCFactory.test.js
+++ test/BCFactory.test.js
@@ -8,6 +8,7 @@ const {
   getBlockNumber,
   increaseBlockTimestampBy,
   getTimestamp,
+  getProofAndIdByIndex,
 } = require("./helpers");
 
 describe("BCFactory", function () {
@@ -139,6 +140,61 @@ describe("BCFactory", function () {
       await genesis.endMinting();
 
       await assertThrowsMessage(factory.connect(wl7).mintGenesis([], false), "PhaseClosedOrNotOpenYet()");
+    });
+  });
+
+  describe("mintOracle", function () {
+    it("should mint oracle", async function () {
+      let ts = (await getTimestamp()) + 1000;
+      await factory.start(ts);
+      // jump to public phase, to simplify testing
+      await increaseBlockTimestampBy(3600 * 25);
+
+      for (let k = 0; k < 4; k++) {
+        for (let i = 1; i < 17; i++) {
+          let wl = i > 12 ? wl4 : i > 8 ? wl3 : i > 4 ? wl2 : wl1;
+          let [proof, id] = getProofAndIdByIndex(wl.address, k * 4 + ((i - 1) % 4));
+          await factory.connect(wl).mintGenesis(id, proof);
+        }
+      }
+
+      await genesis.connect(wl2)["safeTransferFrom(address,address,uint256)"](wl2.address, wl1.address, 6);
+      await genesis.connect(wl3)["safeTransferFrom(address,address,uint256)"](wl3.address, wl1.address, 11);
+      await genesis.connect(wl4)["safeTransferFrom(address,address,uint256)"](wl4.address, wl1.address, 13);
+
+      expect(await genesis.balanceOf(wl1.address)).equal(19);
+
+      await expect(factory.connect(wl1).mintOracle(1, 6, 11, 53)).revertedWith("NotGenesisOwner()");
+      await expect(factory.connect(wl1).mintOracle(1, 6, 11, 13)).revertedWith("NotAFullSet()");
+      await expect(factory.connect(wl1).mintOracle(1, 6, 11, 49)).revertedWith("NotAllSameRarity()");
+
+      await expect(factory.connect(wl1).mintOracle(2, 6, 11, 13))
+        .emit(factory, "OracleMinted")
+        .withArgs(1, 2, 6, 11, 13)
+        .emit(genesis, "Transfer")
+        .withArgs(wl1.address, addr0, 2)
+        .emit(genesis, "Transfer")
+        .withArgs(wl1.address, addr0, 6)
+        .emit(genesis, "Transfer")
+        .withArgs(wl1.address, addr0, 11)
+        .emit(genesis, "Transfer")
+        .withArgs(wl1.address, addr0, 13);
+
+      //check if the parts are burned
+      expect(await genesis.balanceOf(wl1.address)).equal(15);
+      expect(await oracle.balanceOf(wl1.address)).equal(1);
+
+      await expect(factory.connect(wl1).mintOracle(2, 6, 11, 13)).revertedWith("ERC721: invalid token ID");
+    });
+  });
+
+  describe("encode/decode", async function () {
+    it("should verify that the rarity by index is correct", async function () {
+      this.timeout(10000);
+      for (let i = 0; i < 10000; i += Math.round(13 * Math.random())) {
+        const rarity = await factory.rarityByIndex(i);
+        expect(rarity).to.equal(indexes[Math.floor(i / 40)]);
+      }
     });
   });
 });
